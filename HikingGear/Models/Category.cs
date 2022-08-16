a90610902ec736c1ad04266c513a42ef302b2cfc
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-08-15 22:53:58 -0400

    style gear create view

--- HikingGear/Models/Category.cs
+++ HikingGear/Models/Category.cs
@@ -9,7 +9,7 @@ namespace HikingGear.Models
       this.JoinEntities = new HashSet<CategoryGear>();
     }
 
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<CategoryGear> JoinEntities { get; set; }
   }
