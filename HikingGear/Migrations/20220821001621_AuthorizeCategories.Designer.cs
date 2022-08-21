0421f0ea55cde53ecb8397d1e91300dbb27378bc
Louis Knolle <knollelw@gmail.com>
2022-08-20 21:51:41 -0400

    gh push

--- HikingGear/Migrations/20220821001621_AuthorizeCategories.Designer.cs
+++ HikingGear/Migrations/20220821001621_AuthorizeCategories.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HikingGear.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HikingGear.Migrations
 {
     [DbContext(typeof(HikingGearContext))]
-    partial class HikingGearContextModelSnapshot : ModelSnapshot
+    [Migration("20220821001621_AuthorizeCategories")]
+    partial class AuthorizeCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HikingGear/Migrations/20220821001621_AuthorizeCategories.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HikingGear.Migrations
+{
+    public partial class AuthorizeCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Categories",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AspNetUsers_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AspNetUsers_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+        }
+    }
+}
