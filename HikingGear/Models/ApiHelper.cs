79230cc8876ddba3552b02fb01c29495d267d5a1
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-09-06 23:43:31 -0400

    finish api methods

--- HikingGear/Models/ApiHelper.cs
+++ HikingGear/Models/ApiHelper.cs
@@ -12,5 +12,40 @@ namespace HikingGear.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"gears/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    //don't think I'll actually need this except for maybe admin role
+    public static async Task Post(string newGearResult)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"gears", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newGearResult);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Put(int id, string newGearResult)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"gears/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newGearResult);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5003/api");
+      RestRequest request = new RestRequest($"gears/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
--- HikingGear/Models/GearResult.cs
+++ HikingGear/Models/GearResult.cs
@@ -7,13 +7,13 @@ namespace HikingGear.Models
 {
   public class GearResult
   {
-    public int GearId { get; set; }
+    public int GearResultId { get; set; }
     public string Family { get; set; }
     public string Brand { get; set; }
     public string Model { get; set; }
     public int Price { get; set; }  
 
-    public static List<GearResult> GetGearResultss()
+    public static List<GearResult> GetGearResults()
     {
       var apiCallTask = ApiHelper.GetAll();
       var result = apiCallTask.Result;
@@ -22,6 +22,34 @@ namespace HikingGear.Models
       List<GearResult> gearResultList = JsonConvert.DeserializeObject<List<GearResult>>(jsonResponse.ToString());
 
       return gearResultList;
+    }
+
+    public static GearResult GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      GearResult gearResult = JsonConvert.DeserializeObject<GearResult>(jsonResponse.ToString());
+
+      return gearResult;
+    }
+
+    public static void Post(GearResult gearResult)
+    {
+      string jsonGearResult = JsonConvert.SerializeObject(gearResult);
+      var apiCallTask = ApiHelper.Post(jsonGearResult);
+    }
+
+    public static void Put(GearResult gearResult)
+    {
+      string jsonGearResult = JsonConvert.SerializeObject(gearResult);
+      var apiCallTask = ApiHelper.Put(gearResult.GearResultId, jsonGearResult);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
     }
   }
 }
